cb2b5138e2f230e514bccecf426dc7048e69344e
chengjingjing123 <mrjing126@126.com>
2020-08-07 21:19:48 +0800

    分支4提交test01

--- src/main/java/com/school/javatest/test01.java
+++ src/main/java/com/school/javatest/test01.java
@@ -10,7 +10,7 @@ cl * @author chengjinging
  * @date 2020/8/7 下午8:54
  */
 public class test01 {
-
+/Users/edz/IdeaProjects/learnMybatis/src/main/java/com/school/javatest/test01.java
     @Test
     public void test0001(){
         Map map=new TreeMap();
@@ -20,7 +20,7 @@ public class test01 {
         map.put(4,"zhaoliu");
 
         System.out.println(map.keySet());
-        System.out.println("22222");
+        System.out.println("4444");
     }
     @Test void test0002(){
 
