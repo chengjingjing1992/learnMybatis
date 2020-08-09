40b5dd632cf80c540c14d2ef50868494cce9e435
chengjingjing123 <mrjing126@126.com>
2020-08-09 09:39:59 +0800

    内部类和外部类变量名称相同

---
+++ src/main/java/com/school/innerClass/Body.java
@@ -0,0 +1,31 @@
+package com.school.innerClass;
+
+/**
+ * @author chengjinging
+ * @date 2020/8/9 上午9:30
+ */
+public class Body {
+
+    String color="黄色";
+    public class Heart{
+        String color ="红色";
+
+        public void getColor(){
+            String color ="黑色";
+            System.out.println(color); // 输出黑色
+            System.out.println(this.color);  // 输出红色
+            /**
+             * Body.this.color
+             */
+            System.out.println(Body.this.color); //输出黄色
+
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+        Body.Heart heart=new Body().new Heart();
+        heart.getColor();
+    }
+}
